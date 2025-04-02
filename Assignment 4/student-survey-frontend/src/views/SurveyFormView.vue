718e0709e84499b800bc1ac1ff3dd58fc647f4af
Hamaad Zuberi <hamaadz@outlook.com>
2025-04-01 20:51:27 -0400

    Implement Vue Router for navigation; add Home, About, Survey Form, and Survey List views; update App.vue to include navigation links and router-view; modify main.js to use the router; update package.json and package-lock.json to include vue-router dependency.

---
+++ Assignment 4/student-survey-frontend/src/views/SurveyFormView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="survey-form">
+      <h1>Student Survey Form</h1>
+      <p>This is where the survey form will go.</p>
+      <!-- Form elements will be added here later -->
+    </div>
+  </template>
+
+  <script>
+  export default {
+    name: 'SurveyFormView'
+  }
+  </script>
+
+  <style scoped>
+  .survey-form {
+    padding: 20px;
+  }
+  </style>
---
+++ Assignment 4/student-survey-frontend/src/views/SurveyListView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="survey-list">
+    <h1>List of Submitted Surveys</h1>
+    <p>This is where the list of surveys will be displayed.</p>
+    <!-- Survey data table/list will be added here later -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SurveyListView'
+}
+</script>
+
+<style scoped>
+.survey-list {
+  padding: 20px;
+}
+</style>
